d1f6d939fbef2724ef9806aa2b1ba63f797e46e3
DarioPenagos <dpenagosv@unal.edu.co>
2023-02-28 01:28:28 -0500

    d

--- televisores/control.py
+++ televisores/control.py
@@ -5,7 +5,7 @@ from televisores import *
 
 class Control:
     def __init__(self, tv):
-        self.tv = tv
+        self.tv = TV("a", True)
     def turnOn(self):
         self.tv.turnOn()
     def turnOff(self):
